847ccff54744ec9f825254990c7b2a8427d8197b
David Carlisle <d.p.carlisle@gmail.com>
2015-03-15 19:33:11 +0000

    unpack longtable.sty

--- longtable/build.lua
+++ longtable/build.lua
@@ -15,15 +15,13 @@ checkengines   = {"pdftex"}
 -- Documentation is standalone
 typesetfiles = {"*.tex"}
 
--- No packed source files
-unpackfiles = {""}
 
 -- Release a TDS-style zip
 packtdszip  = true
 
--- tests fo this bundle
+-- tests for this bundle
 -- testfiledir = "testfiles"
-testfiledir = ""
+
 
 -- Find and run the build system
 kpse.set_program_name ("kpsewhich")
